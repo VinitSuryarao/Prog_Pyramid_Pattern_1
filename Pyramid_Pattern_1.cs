fe4bd94989c40756a2999d5c39cce40537602c06
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:05:23 +0530

    Add files via upload

---
+++ Pyramid_Pattern_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //  Pyramid Pattern
+        //  *
+        //  **
+        //  ***
+        //  ****
+        //  *****
+        //  ******
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+
+
